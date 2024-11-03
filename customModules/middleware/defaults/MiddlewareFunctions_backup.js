2a0082d6f16363a201c07da2cd536042ce6342b1
Jesse <jessealexanderthompson@gmail.com>
2024-11-03 17:10:26 -0500

    Update middleware func

--- customModules/middleware/defaults/MiddlewareFunctions_backup.js
+++ customModules/middleware/defaults/MiddlewareFunctions_backup.js
@@ -1,3 +1,11 @@
+
+  /**
+* Paints meta based on incoming object
+* @param {Router} router
+* @param {Object} items - SEO tag objects
+* @param {Object} props
+* @returns 
+*/
 const transformMeta = (router, items, props) => {
     // Manipulate Items here to control your meta strategy.
     // Use router to determine what page you are on.
@@ -5,9 +13,14 @@ const transformMeta = (router, items, props) => {
     return items
 }
 
+const manualAllowResourceAuth = () => {
+
+  return true
+}
 
 const functions = {
-    transformMeta
+  manualAllowResourceAuth,
+  transformMeta
 }
 
 export default functions
